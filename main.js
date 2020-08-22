16b940f98142f63e1a9ed97a29617d8614f7c6e1
Arsini <a3323221b@163.com>
2020-08-22 13:44:17 +0800

    main

--- main.js
+++ main.js
@@ -1,8 +1,37 @@
+/**
+ * root: admin/3323221lzc
+ * master: ethanol/3323221lzc
+ */
+// mongod --auth --dbpath /usr/local/mongodb/data/db --logpath /usr/local/mongodb/log/mongodb.log --fork
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true}, err => {
+const URL = 'mongodb://localhost:27017/master'
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'master',
+    user: 'ethanol',
+    pass: '3323221lzc',
+}
+const cb = err => {
     if(err){
         console.log(err)
         return
     }
     console.log('successful')
-})
+}
+mongoose.connect(URL, options, cb)
+// mongoose.connect(
+//     'mongodb://localhost:27017/test', 
+//     {
+//         useNewUrlParser: true, 
+//         useUnifiedTopology: true,
+//     }, 
+//     err => {
+//         if(err){
+//             console.log(err)
+//             return
+//         }
+//         console.log('successful')
+//     }
+// )
+

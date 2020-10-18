f8d06850cff9ab4e54e50b5109a79594941ff043
korn <korn3323221@gmail.com>
2020-10-18 20:37:08 +0800

    updata

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const global = require(`${__dirname}/app_global`)
+const global = require(`${__dirname}/app_global`)//设置全局属性
 const express = require('express')
 const bodyParser = require('body-parser');//解析,用req.body获取post参数
 const app = express()
@@ -6,9 +6,10 @@ const app = express()
       app.use(bodyParser.urlencoded({extended: false}))
 const port = appG.config.port
 const user = require(`${appG.RP}/router/user`)
-
+const { getIPAddress } = require('./utils')
 app.use('/user', user)
-
+app.use(express.static('./www'))
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`listening at http://localhost:${ port }`)
+    console.log(`listening at http://${ getIPAddress() }:${port}`)
 })
